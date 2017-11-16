f5291e49a7ecc40f841147f8b1f8316b8aedf2b8
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-16 17:48:09 +0100

    Initialize twitter

---
+++ twitter_data/twitter_statistics.py
@@ -0,0 +1,8 @@
+import twitter
+
+api = twitter.Api(consumer_key='TjtC3Moxczg62gsMYNzrxGtQ3',
+                  consumer_secret='rObL9LQu7pH0iU1DmvRvYiNpwzqBKtHQfWLJxBbuEfyvroAxB6',
+                  access_token_key='913148816668454912-ir0ibXXXO0h2NU4PZhYIPm42ZbtNflf',
+                  access_token_secret='hyTdLdsIGHaOMnPVQIaHEr3KCeThW4hlPK3BXTUkKthd6')
+
+print(api.VerifyCredentials())
