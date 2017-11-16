6379ed33d1cec747899afe1685b5ab44154ab477
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-16 18:37:14 +0100

    Create wordcloud from timeline tweets

--- twitter_data/twitter_statistics.py
+++ twitter_data/twitter_statistics.py
@@ -4,6 +4,7 @@ from PIL import Image
 from os import path
 import matplotlib.pyplot as plt
 import random
+import json
 
 from wordcloud import WordCloud, STOPWORDS
 
@@ -16,42 +17,40 @@ print(api.VerifyCredentials())
 print(api.GetFriends())
 print(api.GetHomeTimeline())
 
+timelineTweets = api.GetHomeTimeline()
+print(type(timelineTweets[0]))
 
-# def grey_color_func(word, font_size, position, orientation, random_state=None,
-#                     **kwargs):
-#     return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
-#
-#
-# d = path.dirname(__file__)
-#
-# # read the mask image
-# mask = np.array(Image.open(path.join(d, "bitcoin_mask.png")))
-#
-# # movie script of "a new hope"
-# # http://www.imsdb.com/scripts/Star-Wars-A-New-Hope.html
-# # May the lawyers deem this fair use.
-# text = open(path.join(d, 'a_new_hope.txt')).read()
-#
-# # preprocessing the text a little bit
-# text = text.replace("HAN", "Han")
-# text = text.replace("LUKE'S", "Luke")
-#
-# # adding movie script specific stopwords
-# stopwords = set(STOPWORDS)
-# stopwords.add("int")
-# stopwords.add("ext")
-#
-# wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
-#                random_state=1).generate(text)
-# # store default colored image
-# default_colors = wc.to_array()
-# plt.title("Custom colors")
-# plt.imshow(wc.recolor(color_func=grey_color_func, random_state=3),
-#            interpolation="bilinear")
-# wc.to_file("a_new_hope.png")
-# plt.axis("off")
-# plt.figure()
-# plt.title("Default colors")
-# plt.imshow(default_colors, interpolation="bilinear")
-# plt.axis("off")
-# plt.show()
+text = ""
+
+for tweetStatus in timelineTweets:
+    j = json.loads(tweetStatus.AsJsonString())
+    text += j['text']
+
+
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+
+
+directory = path.dirname(__file__)
+
+# read the mask image
+mask = np.array(Image.open(path.join(directory, "bitcoin_mask.png")))
+
+# # adding specific stopwords
+stopwords = set(STOPWORDS)
+stopwords.add("https")
+
+wordCloud = WordCloud(max_words=1000, mask=mask, margin=10, stopwords=stopwords, random_state=1).generate(text)
+# store default colored image
+default_colors = wordCloud.to_array()
+plt.title("Custom colors")
+plt.imshow(wordCloud.recolor(color_func=grey_color_func, random_state=3),
+           interpolation="bilinear")
+wordCloud.to_file("cryptocurrencies.png")
+plt.axis("off")
+plt.figure()
+plt.title("Default colors")
+plt.imshow(default_colors, interpolation="bilinear")
+plt.axis("off")
+plt.show()
